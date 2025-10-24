97b7c941b08b3b18d4ee58b6e34def6555d0c127
SamuelItaire <20103764@mail.wit.ie>
2025-10-24 16:02:09 +0100

    Added Top Rated page.

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 jsx
-import TopRatedMoviesPage from './pages/topRatedMoviesPage';
+import TopRatedMoviesPage from 'src/pages/topRatedMoviesPage';
 
 export default defineConfig([
   globalIgnores(['dist']),
